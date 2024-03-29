65b90ab84001291d404f457751af71c6e53fc69c
Stonewms0404 <48721922+Stonewms0404@users.noreply.github.com>
2024-03-28 21:30:44 -0400

    Raycasts?

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,17 +9,23 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     [Tooltip("Player inputs")]
     PlayerInput PInput;
+    [SerializeField]
+    [Tooltip("Player Collider")]
+    Collider coll;
+    [SerializeField]
+    [Tooltip("Raycast Emitters")]
+    GameObject frontEmitter, backEmitter;
 
     [Header("Variables")]
     [SerializeField]
     [Tooltip("Player Speed")]
     float speed;
-    [Header("Raycasts")]
-    [SerializeField]
-    RaycastHit aboveHit, frontHit, upHit, downHit, backHit;
+
+    Ray upHit, frontHit, upDiagHit, downDiagHit, backHit;
+    bool canMove, canClimbUp, canClimbDown, canGrab, canLedge;
 
     Vector3Int adjustedPlayerPos;
-    Vector3 direction = Vector3.zero;
+    Vector3 direction = Vector3.forward;
 
     private void Awake()
     {
@@ -29,20 +35,37 @@ public class PlayerMovement : MonoBehaviour
     private void OnBasicMovement(InputValue value)
     {
         Vector2 temp = value.Get<Vector2>();
-        direction = new(temp.x, direction.x , temp.y);
     }
 
     private void FixedUpdate()
     {
-        Debug.Log(direction);
-        if (direction == Vector3.zero)
-        {
-            transform.position += direction;
-        }
+        MovementRaycast();
     }
 
     void MovementRaycast()
     {
+        CheckFront(out RaycastHit frontCast);
+        frontCast.distance = 1;
+        if (frontCast.collider != null)
+        {
+            upHit.origin = frontEmitter.transform.position;
+            upHit.direction = Vector3.up;
+            coll.Raycast(upHit, out RaycastHit upCast, 1);
+            Debug.Log("Hit " + frontHit.GetPoint(1));
+            //if (frontCast.collider.tag == "Block")
+        }
+        else
+        {
+            Debug.Log("Not Hit " + frontHit.GetPoint(1));
+        }
+    }
 
+    RaycastHit CheckFront(out RaycastHit frontCast)
+    {
+        frontHit.origin = frontEmitter.transform.position;
+        frontHit.direction = Vector3.forward;
+        coll.Raycast(frontHit, out frontCast, 1);
+
+        return frontCast;
     }
 }
